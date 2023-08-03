d60e0f9886b147d8e3b6fb4b33f58011f9ffcdb6
Nahuel Oliva <luis.oliva@est.fi.uncoma.edu.ar>
2023-08-02 22:10:45 -0300

    Errores Corregidos

--- src/componentes/productos/Producto.jsx
+++ src/componentes/productos/Producto.jsx
@@ -3,20 +3,19 @@ import Imagen from "../imagenes/Imagen"
 import "./producto.css"
 import Heart from "../corazon-tienda/Heart";
 let stock =0;
-let id = 0;
 export default function Producto(props) {
-    id = id + 1
+    stock = props.cantidad;
     return(
         <div className="producto">
                 <Heart/>
                 <br/>
                 <Imagen 
-                    key={id}
+                    key={props.url}
                     class={props.class}
                     img={-1}
                     url={props.url}
                 />
-                {stock = props.cantidad}
+                
                 <h4>{props.nombre}</h4>
 
         </div>
